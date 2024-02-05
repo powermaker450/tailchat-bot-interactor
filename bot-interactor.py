837727480f0170c5f853365ff277dbdddf793b4c
powermaker450 <powermaker450@proton.me>
2024-02-05 12:10:22 -0500

    Catch more errors
    Catch and categorize more errors that might occur at runtime

--- bot-interactor.py
+++ bot-interactor.py
@@ -1,37 +1,64 @@
 #!/usr/bin/env python3
 try:
 	import requests
-except:
-	print("Error: The requests module is not installed.")
+except ModuleNotFoundError:
+	print("ERROR: The requests module is not installed.")
 	exit()
+except:
+	print("ERROR: Unknown error")
+	exit()
+
 try:
 	import env
+except ModuleNotFoundError:
+	print("ERROR: The env.py file appears to be missing. It needs to be in the same directory as the program.")
+	exit()
+except SyntaxError:
+	print("ERROR: The env.py file appears to be formatted incorrectly. Check and fix any errors.")
+	exit()
 except:
-	print("Error: The env.py file appears to be missing. It needs to be in the same directory as the program.")
+	print("ERROR: Unknown error")
 	exit()
 
 #don't set the values here. edit the env.py file.
-key=env.key
-instance_url=env.instance_url
-guild_name=env.guild_name
-channel_name=env.channel_name
+try:
+	key=env.key
+	instance_url=env.instance_url
+	guild_name=env.guild_name
+	channel_name=env.channel_name
+except AttributeError:
+	print("ERROR: Some variables have not been created in the env.py file. Check and fix any errors.\n")
+	print("The following variables should be defined: \"key\", \"instance_url\".")
+	print("Optionally, these can also be defined: \"guild_name\" \"channel_name\".")
+	exit()
+except:
+	print("ERROR: Unknown error")
+	exit()
 
 in_use=True
 blockedtext="*"*20
 
 #functions
 def post_message(instance,apikey,msg):
-	requests.post("{}/api/plugin:com.msgbyte.simplenotify/webhook/callback?subscribeId={}&text={}".format(instance,apikey,msg))
+	"""Attempts to post a message in a Tailchat channel
+	given an instance url, bot key, and message content.
+	If the POST request fails, it will throw an error and close the program.
+	"""
+	try:
+		requests.post("{}/api/plugin:com.msgbyte.simplenotify/webhook/callback?subscribeId={}&text={}".format(instance,apikey,msg))
+	except:
+		print("ERROR: Server not found. Are you sure you typed in the correct instance URL in the env.py file?")
+		exit()
 
 #check for required fields
 if key=="":
-	print("Error: You need to input the bot key by editing the env.py file.")
+	print("ERROR: You need to input the bot key by editing the env.py file.")
 	exit()
 if len(key)!=24:
-	print("Error: Bot key appears to be invalid. Try checking if it is inputted correctly.")
+	print("ERROR: Bot key appears to be invalid. Try checking if it is inputted correctly.")
 	exit()
 if instance_url=="":
-	print("Error: You need to input the server URL by editing the env.py file.")
+	print("ERROR: You need to input the server URL by editing the env.py file.")
 	exit()
 
 #if both guild_name and channel_name are filled out in the env.py file, the program will print the guild name and channel name respectively.
